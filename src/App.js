f17c59471b69b3e49d2c278ccafe2fdd878aed60
Govinda <govinda777@gmail.com>
2024-11-23 08:49:55 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// src/App.js
 import React from 'react';
+import SwaggerUI from './components/SwaggerUI';
 
-const App = () => {
+function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Swagger UI App</h1>
+    <div className="App">
+      <SwaggerUI />
     </div>
   );
-};
+}
 
 export default App;
